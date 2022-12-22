8089e9853bee62515c6fe4b363131256eb21889d
pc64wayprog <pc64wayprog@pc64wayprog-PC>
2022-12-22 17:42:21 -0300

    first commit
    -create maven project
    - hola mundo

---
+++ src/main/java/ar/com/codoacodo/main.java
@@ -0,0 +1,10 @@
+package ar.com.codoacodo;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("es el primero");
+	}
+
+}
